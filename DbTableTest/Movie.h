7bdbc16086cb12fcdf2432b411ed77310af43a21
saerem21 <nemessisdds>
2014-01-13 18:15:46 +0900

    디비 테이블 테스트입니.

---
+++ DbTableTest/Movie.h
@@ -0,0 +1,16 @@
+//
+//  Movie.h
+//  DbTableTest
+//
+//  Created by SDT-1 on 2014. 1. 13..
+//  Copyright (c) 2014년 SDT-1. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Movie : NSObject
+
+@property (copy,nonatomic)NSString *title;
+@property int rowID;
+
+@end
---
+++ DbTableTest/Movie.m
@@ -0,0 +1,13 @@
+//
+//  Movie.m
+//  DbTableTest
+//
+//  Created by SDT-1 on 2014. 1. 13..
+//  Copyright (c) 2014년 SDT-1. All rights reserved.
+//
+
+#import "Movie.h"
+
+@implementation Movie
+
+@end
